393c30fe28099f692bebec58a96d25bedba9b51d
Jasper Tey <jasper.tey@gmail.com>
2023-10-22 20:04:33 -0400

    Normalize path expectations.

--- tests/Support/DomainTest.php
+++ tests/Support/DomainTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Lunarstorm\LaravelDDD\Support\Domain;
+use Lunarstorm\LaravelDDD\Support\Path;
 
 it('can initialize a domain', function ($domainName, $subdomainName) {
     $domain = new Domain($domainName, $subdomainName);
@@ -23,7 +24,7 @@ it('can describe a domain model', function ($domainName, $name, $expectedFQN, $e
     expect((new Domain($domainName))->model($name))
         ->name->toBe($name)
         ->fqn->toBe($expectedFQN)
-        ->path->toBe($expectedPath);
+        ->path->toBe(Path::normalize($expectedPath));
 })->with([
     ['Reporting', 'InvoiceReport', 'Domain\\Reporting\\Models\\InvoiceReport', 'src/Domain/Reporting/Models/InvoiceReport.php'],
     ['Reporting.Internal', 'InvoiceReport', 'Domain\\Reporting\\Internal\\Models\\InvoiceReport', 'src/Domain/Reporting/Internal/Models/InvoiceReport.php'],
@@ -33,7 +34,7 @@ it('can describe a domain factory', function ($domainName, $name, $expectedFQN, 
     expect((new Domain($domainName))->factory($name))
         ->name->toBe($name)
         ->fqn->toBe($expectedFQN)
-        ->path->toBe($expectedPath);
+        ->path->toBe(Path::normalize($expectedPath));
 })->with([
     ['Reporting', 'InvoiceReportFactory', 'Database\\Factories\\Reporting\\InvoiceReportFactory', 'database/factories/Reporting/InvoiceReportFactory.php'],
     ['Reporting.Internal', 'InvoiceReportFactory', 'Database\\Factories\\Reporting\\Internal\\InvoiceReportFactory', 'database/factories/Reporting/Internal/InvoiceReportFactory.php'],
@@ -43,7 +44,7 @@ it('can describe a data transfer object', function ($domainName, $name, $expecte
     expect((new Domain($domainName))->dataTransferObject($name))
         ->name->toBe($name)
         ->fqn->toBe($expectedFQN)
-        ->path->toBe($expectedPath);
+        ->path->toBe(Path::normalize($expectedPath));
 })->with([
     ['Reporting', 'InvoiceData', 'Domain\\Reporting\\Data\\InvoiceData', 'src/Domain/Reporting/Data/InvoiceData.php'],
     ['Reporting.Internal', 'InvoiceData', 'Domain\\Reporting\\Internal\\Data\\InvoiceData', 'src/Domain/Reporting/Internal/Data/InvoiceData.php'],
@@ -53,7 +54,7 @@ it('can describe a view model', function ($domainName, $name, $expectedFQN, $exp
     expect((new Domain($domainName))->viewModel($name))
         ->name->toBe($name)
         ->fqn->toBe($expectedFQN)
-        ->path->toBe($expectedPath);
+        ->path->toBe(Path::normalize($expectedPath));
 })->with([
     ['Reporting', 'InvoiceReportViewModel', 'Domain\\Reporting\\ViewModels\\InvoiceReportViewModel', 'src/Domain/Reporting/ViewModels/InvoiceReportViewModel.php'],
     ['Reporting.Internal', 'InvoiceReportViewModel', 'Domain\\Reporting\\Internal\\ViewModels\\InvoiceReportViewModel', 'src/Domain/Reporting/Internal/ViewModels/InvoiceReportViewModel.php'],
@@ -63,7 +64,7 @@ it('can describe a value object', function ($domainName, $name, $expectedFQN, $e
     expect((new Domain($domainName))->valueObject($name))
         ->name->toBe($name)
         ->fqn->toBe($expectedFQN)
-        ->path->toBe($expectedPath);
+        ->path->toBe(Path::normalize($expectedPath));
 })->with([
     ['Reporting', 'InvoiceTotal', 'Domain\\Reporting\\ValueObjects\\InvoiceTotal', 'src/Domain/Reporting/ValueObjects/InvoiceTotal.php'],
     ['Reporting.Internal', 'InvoiceTotal', 'Domain\\Reporting\\Internal\\ValueObjects\\InvoiceTotal', 'src/Domain/Reporting/Internal/ValueObjects/InvoiceTotal.php'],
@@ -73,7 +74,7 @@ it('can describe an action', function ($domainName, $name, $expectedFQN, $expect
     expect((new Domain($domainName))->action($name))
         ->name->toBe($name)
         ->fqn->toBe($expectedFQN)
-        ->path->toBe($expectedPath);
+        ->path->toBe(Path::normalize($expectedPath));
 })->with([
     ['Reporting', 'SendInvoiceReport', 'Domain\\Reporting\\Actions\\SendInvoiceReport', 'src/Domain/Reporting/Actions/SendInvoiceReport.php'],
     ['Reporting.Internal', 'SendInvoiceReport', 'Domain\\Reporting\\Internal\\Actions\\SendInvoiceReport', 'src/Domain/Reporting/Internal/Actions/SendInvoiceReport.php'],
