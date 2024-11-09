deecd2cd0d8e357939f518b4c8952817e37d62c7
Jasper Tey <jasper.tey@gmail.com>
2024-11-09 16:11:04 -0500

    Make methods chainable.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -136,6 +136,8 @@ class TestCase extends Orchestra
         (new Process($command, base_path(), ['COMPOSER_MEMORY_LIMIT' => '-1']))
             ->setTimeout(null)
             ->run(function ($type, $output) {});
+
+            return $this;
     }
 
     protected function cleanSlate()
@@ -157,11 +159,15 @@ class TestCase extends Orchestra
         File::deleteDirectory(app_path('Models'));
 
         File::deleteDirectory(base_path('bootstrap/cache/ddd'));
+
+        return $this;
     }
 
     protected function cleanStubs()
     {
         File::cleanDirectory(base_path('stubs'));
+
+        return $this;
     }
 
     protected function setupTestApplication()
@@ -173,6 +179,8 @@ class TestCase extends Orchestra
         File::ensureDirectoryExists(app_path('Models'));
 
         $this->setDomainPathInComposer('Domain', 'src/Domain');
+
+        return $this;
     }
 
     protected function setDomainPathInComposer($domainNamespace, $domainPath, bool $reload = true)
