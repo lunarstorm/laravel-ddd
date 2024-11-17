b9755b92bcbb08633556c1033badc9254ada33ec
Jasper Tey <jasper.tey@gmail.com>
2024-11-17 17:06:23 -0500

    Revert

--- tests/Factory/DomainFactoryTest.php
+++ tests/Factory/DomainFactoryTest.php
@@ -31,7 +31,11 @@ it('is backwards compatible with factories located in database/factories/**/*', 
 it('can instantiate a domain model factory', function ($domainParameter, $modelName, $modelClass) {
     $this->setupTestApplication();
 
-    config()->set([
+    // $this->afterApplicationRefreshed(function () {
+    //     app('ddd.autoloader')->boot();
+    // });
+
+    $this->refreshApplicationWithConfig([
         'ddd.base_model' => 'Lunarstorm\LaravelDDD\Models\DomainModel',
         'ddd.autoload.factories' => true,
     ]);
