d5e049997f3767a092c9f9c1b5ee86bd12370a33
Jasper Tey <jasper.tey@gmail.com>
2024-11-17 14:15:11 -0500

    Normalize paths for windows.

--- tests/Support/AutoloaderTest.php
+++ tests/Support/AutoloaderTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Lunarstorm\LaravelDDD\Support\AutoloadManager;
+use Lunarstorm\LaravelDDD\Support\Path;
 
 beforeEach(function () {
     $this->setupTestApplication();
@@ -29,19 +30,19 @@ beforeEach(function () {
         'cache.default' => 'file',
     ]);
 
+    $this->expectedPaths = collect([
+        app()->basePath('src/Domain'),
+        app()->basePath('src/Application'),
+        app()->basePath('src/Infrastructure'),
+        app()->basePath('src/Support'),
+        app()->basePath('lib'),
+    ])->map(fn ($path) => Path::normalize($path))->toArray();
+
     $this->setupTestApplication();
 });
 
 it('can discover paths to all layers', function () {
     $autoloader = app(AutoloadManager::class);
 
-    $expected = [
-        app()->basePath('src/Domain'),
-        app()->basePath('src/Application'),
-        app()->basePath('src/Infrastructure'),
-        app()->basePath('src/Support'),
-        app()->basePath('lib'),
-    ];
-
-    expect($autoloader->getAllLayerPaths())->toEqualCanonicalizing($expected);
+    expect($autoloader->getAllLayerPaths())->toEqualCanonicalizing($this->expectedPaths);
 });
