bd4e4a46e52ff89ca0f6b9f2366a1bb95fe810f8
Ivan Polák <ivan.polak25@gmail.com>
2025-06-28 20:25:54 +0200

    changed search of files to delete to use filesystem instead of shell find to achieve cross-platform compatibility and to make the code safer

--- src/Listeners/MigrationsPrunedSubscriber.php
+++ src/Listeners/MigrationsPrunedSubscriber.php
@@ -5,6 +5,9 @@ namespace Lunarstorm\LaravelDDD\Listeners;
 use Illuminate\Database\Events\MigrationsPruned;
 use Illuminate\Events\Dispatcher;
 use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+use SplFileInfo;
 
 class MigrationsPrunedSubscriber
 {
@@ -14,45 +17,45 @@ class MigrationsPrunedSubscriber
     {
         $locations = [
             config('ddd.domain_path'),
-            ...config('ddd.layers')
+            ...config('ddd.layers'),
         ];
 
         $migrationDirs = collect();
+        /** @var Collection<int, SplFileInfo> $filesToDelete */
+        $filesToDelete = collect();
+
+        $filesystem = new Filesystem;
 
         foreach ($locations as $location) {
-            $command = 'find ' . base_path("$location/**/Database") . ' -type d -name "Migrations" 2>/dev/null';
-            // 2>/dev/null at the end is to ignore warnings such as: find: /.../src/Infrastructure/**/Database: No such file or directory
-
-            $result = collect(shell_exec(
-                $command
-            ) ?? []);
-
-            $migrationDirs->push(
-                ...explode(PHP_EOL, $result[0] ?? '')
-            );
-        }
-
-        $migrationDirs = $migrationDirs
-            ->map(static fn (string $dir): string => trim($dir))
-            ->filter(static fn (string $dir): bool => ! empty($dir))
-            ->map(static fn (string $dir): string => str_replace(base_path('/'), '', $dir));
-
-        foreach ($migrationDirs as $dir) {
-            // we could delete the directories, but that could cause damage if a bug happened,
-            // instead we'll just delete php files and only delete the directory afterwards if it is empty
-
-            $filesystem = new Filesystem;
-
-            $files = $filesystem->files($dir);
-
-            foreach ($files as $file) {
-                if ($file->getExtension() == 'php') {
-                    $filesystem->delete("$file");
-                }
+            if (! $filesystem->exists($location)) {
+                continue;
             }
 
-            if ($filesystem->isEmptyDirectory($dir)) {
-                $filesystem->deleteDirectory($dir);
+            $allFiles = $filesystem->allFiles($location);
+
+            foreach ($allFiles as $file) {
+                if (
+                    Str::endsWith($file->getPath(), 'Database/Migrations')
+                    &&
+                    $file->getExtension() == 'php'
+                ) {
+                    $filesToDelete->push($file);
+                }
+            }
+        }
+
+        /** @var Collection<string, Collection<int, SplFileInfo>> $groupedFilesToDelete */
+        $groupedFilesToDelete = $filesToDelete->groupBy(function (SplFileInfo $file) {
+            return $file->getPath();
+        });
+
+        foreach ($groupedFilesToDelete as $location => $groupOfFilesToDelete) {
+            foreach ($groupOfFilesToDelete as $file) {
+                $filesystem->delete($file->getPathname());
+            }
+
+            if ($filesystem->isEmptyDirectory($location)) {
+                $filesystem->deleteDirectory($location);
             }
         }
     }
