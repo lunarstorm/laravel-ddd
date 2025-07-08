f8162a95446048bef211aa484a8b256d922afa25
Jasper Tey <jasper.tey@gmail.com>
2025-07-08 09:12:02 -0400

    Add test coverage and introduce DomainMigrationsPruned event.

---
+++ src/Events/DomainMigrationsPruned.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Lunarstorm\LaravelDDD\Events;
+
+use Illuminate\Database\Events\MigrationsPruned;
+
+class DomainMigrationsPruned extends MigrationsPruned
+{
+
+}
--- src/Listeners/MigrationsPrunedSubscriber.php
+++ src/Listeners/MigrationsPrunedSubscriber.php
@@ -5,17 +5,20 @@ namespace Lunarstorm\LaravelDDD\Listeners;
 use Illuminate\Database\Events\MigrationsPruned;
 use Illuminate\Events\Dispatcher;
 use Illuminate\Filesystem\Filesystem;
+use Lunarstorm\LaravelDDD\Events\DomainMigrationsPruned;
 use Lunarstorm\LaravelDDD\Support\DomainMigration;
 
 class MigrationsPrunedSubscriber
 {
-    public function handle(): void
+    public function handle(MigrationsPruned $event): void
     {
         $migrationDirs = DomainMigration::paths();
         $filesystem = new Filesystem;
 
-        foreach ($migrationDirs as $migrationDir) {
-            $filesystem->deleteDirectory($migrationDir, preserve: false);
+        foreach ($migrationDirs as $path) {
+            $filesystem->deleteDirectory($path, preserve: false);
+
+            event(new DomainMigrationsPruned($event->connection, $path));
         }
     }
 
