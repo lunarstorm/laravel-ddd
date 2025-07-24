5aa44ba48da46fb4cdbfb9913ec9e3be58712c03
Benjamin <panyamin@users.noreply.github.com>
2025-07-24 12:02:10 +0200

    docs: Add generic Factory on HasDomainFactory trait

--- src/Factories/HasDomainFactory.php
+++ src/Factories/HasDomainFactory.php
@@ -4,10 +4,16 @@ namespace Lunarstorm\LaravelDDD\Factories;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+/**
+ * @template TFactory of \Illuminate\Database\Eloquent\Factories\Factory
+ */
 trait HasDomainFactory
 {
     use HasFactory;
 
+    /**
+     * @return ?TFactory
+     */
     protected static function newFactory()
     {
         return DomainFactory::factoryForModel(get_called_class());
